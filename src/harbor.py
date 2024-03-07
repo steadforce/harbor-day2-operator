6281694dfa602b7a875ed229887ad79edd5d334a
David Peters <david.peters@steadforce.com>
2024-03-07 10:19:52 +0100

    fix missing closing bracket

--- src/harbor.py
+++ src/harbor.py
@@ -149,7 +149,7 @@ async def sync_registries(target_registries: [Registry]):
 
 
 async def construct_full_robot_name(target_robot: Robot) -> str:
-    if (namespace := target_robot['permissions'][0]['namespace'] != '*':
+    if (namespace := target_robot['permissions'][0]['namespace']) != '*':
         return f'{robot_name_prefix}{namespace}+{target_robot.name}'
     else:
         return f'{robot_name_prefix}{target_robot.name}'
