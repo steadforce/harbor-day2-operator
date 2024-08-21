783d4e42b37851ea08edaea88ccc7f06c6671861
David Peters <57600507+peters-david@users.noreply.github.com>
2024-08-21 16:41:13 +0200

    Remove password checking (#34)
    * added check if client credentials are working
    
    * newline for linter
    
    * update on client
    
    * print more information
    
    * added missing await and removed user check
    
    * get current user
    
    * print more necessary information
    
    * change error message on unauthorized

--- src/harbor.py
+++ src/harbor.py
@@ -55,7 +55,7 @@ async def main() -> None:
     print("")
 
     # Update admin password
-    print("UPDATE ADMIN PASSWROD")
+    print("UPDATE ADMIN PASSWORD")
     await sync_admin_password()
     print("")
 
@@ -473,8 +473,9 @@ async def wait_until_healthy() -> None:
         sleep(5)
 
 
-async def sync_admin_password() -> None:
+async def update_password() -> None:
     try:
+        print("Updating password")
         old_password_client = HarborAsyncClient(
             url=api_url,
             username=admin_username,
@@ -491,9 +492,16 @@ async def sync_admin_password() -> None:
         print("- Updated admin password")
     except Unauthorized:
         print(
-            "- Admin password remains unchanged since it is does not match the"
-            "  old admin password password"
+            "  => ERROR: Unable to change the admin password."
+            "  Neither the old nor the new password are correct."
         )
+
+
+async def sync_admin_password() -> None:
+    try:
+        await client.get_current_user()
+    except Unauthorized:
+        await update_password()
 
 
 def get_member_id(members: [ProjectMemberEntity], username: str) -> int | None:
