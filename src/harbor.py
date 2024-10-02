a2239623601feb798d1b01170bf33b836a3182bb
David Peters <david.peters@steadforce.com>
2024-10-02 09:07:09 +0200

    print update/create of retention policies

--- src/harbor.py
+++ src/harbor.py
@@ -197,13 +197,13 @@ async def sync_retention_policies(retention_policies: [RetentionPolicy]):
             project_retention_id = await client.get_project_retention_id(
                 retention_scope
             )
-            print("Updating retention policy")
             await client.update_retention_policy(
                 project_retention_id,
                 retention_policy
             )
+            print("Updating retention policy for scope " + retention_scope)
         except Exception as e:
-            print("Creating retention policy")
+            print("Creating retention policy for scope " + retention_scope)
             await client.create_retention_policy(
                 retention_policy
             )
