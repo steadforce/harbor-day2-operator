2bdd0339e5d850b670876709c5dcd96faf49c2cd
David Peters <57600507+peters-david@users.noreply.github.com>
2024-10-23 10:01:24 +0200

    Separate `harbor.py` into modules and adapt `nuitka` (#53)
    * spread modules
    
    * nuitka mode change
    
    * fix imports and successful nuitka build
    
    * extract file check
    
    * convert to asynchronous functions
    
    * imports in separated files
    
    * util functions
    
    * harbor main file doc
    
    * linter linebreaks and newlines
    
    * remove trailing whitespace
    
    * fix missing import
    
    * fix missing newline
    
    * nuitka onefile option
    
    * remove global client
    
    * fix line linter
    
    * add missing import
    
    * extract path
    
    * fix missing and unneeded imports
    
    * fix missing and unneeded imports of separated files
    
    * changed positions of env vars
    
    * updated import
    
    * moved enum
    
    * enum import to file
    
    * rename variable to new name
    
    * add new_admin_password to utils

---
+++ src/purge_job_schedule.py
@@ -0,0 +1,19 @@
+import json
+
+
+async def sync_purge_job_schedule(client, path):
+    """Synchronize the purge job and its schedule
+
+    The purge job and its schedule from the purge job schedule file,
+    if existent, will be updated and applied to harbor.
+    """
+
+    print("SYNCING PURGE JOB SCHEDULE")
+    purge_job_schedule = json.load(open(path))
+    try:
+        await client.get_purge_job_schedule()
+        print("Updating purge job schedule")
+        await client.update_purge_job_schedule(purge_job_schedule)
+    except Exception as e:
+        print("Creating purge job schedule")
+        await client.create_purge_job_schedule(purge_job_schedule)
