1e6a15dca49f8624f5bbcdc2597d8bee801fda0a
David Peters <david.peters@steadforce.com>
2024-11-07 15:43:30 +0100

    json load from string instead of file

--- src/projects.py
+++ src/projects.py
@@ -11,7 +11,7 @@ async def sync_projects(client, path):
 
     print("SYNCING PROJECTS")
     target_projects_string = await fill_template(client, path)
-    target_projects = json.load(target_projects_string)
+    target_projects = json.loads(target_projects_string)
     current_projects = await client.get_projects(limit=None)
     current_project_names = [
         current_project.name for current_project in current_projects
